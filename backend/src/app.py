2f1e69ec98b3dfa0a7d62d00af046479c78d0ea9
DORSEUIL Mathéo <m.dorseuil@rt-iut.re>
2025-04-15 15:47:35 +0000

    add connection to an database and fetching data to display then on backend. Nothing changed on frontend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,31 +1,50 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
+import psycopg2
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="supportdb",
+        user="postgres",
+        password="motdepasse123",
+        host="localhost",
+        port="5433"
+    )
+    return conn
 
 app = Flask(__name__)
 app.secret_key = "dev"
 
-# Données fictives
-tickets = [
-    {
-        'id': 1,
-        'objet': 'Connexion impossible',
-        'description': 'Je ne peux pas me connecter',
-        'email': 'exemple1@mail.com',
-        'piece_jointe': '',
-    },
-    {
-        'id': 2,
-        'objet': 'Erreur de facturation',
-        'description': 'Facture incorrecte',
-        'email': 'exemple2@mail.com',
-        'piece_jointe': 'https://via.placeholder.com/100',
-    },
-]
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        flash("Formulaire soumis ! (fonctionnalité non active)")
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM tickets;')
+    tickets = cur.fetchall()
+    cur.close()
+    conn.close()
     return render_template('index.html', tickets=tickets)
+
+@app.route("/mark_finished/<int:ticket_id>", methods=["POST"])
+def mark_finished(ticket_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("UPDATE tickets SET statut = %s WHERE id = %s", ("fini", ticket_id))
+    flash("Le statut a bien été changer")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect(url_for("index"))
+
+@app.route("/delete_ticket/<int:ticket_id>", methods=["POST"])
+def delete_ticket(ticket_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM tickets WHERE id = %s", (ticket_id,))
+    flash("Ticket supprimé avec succès.")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
