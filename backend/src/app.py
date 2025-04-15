1b689e394ee66e4d4f2bf76fc078c83f2e32b01c
DORSEUIL Mathéo <m.dorseuil@rt-iut.re>
2025-04-15 10:07:48 +0000

    add Backend demo page

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,21 +1,11 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template
 
-
-# instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
 
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
-
-
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
-
+@app.route('/')
+def home():
+    return render_template('index.html', title="Démo Flask", name="Mathéo")
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True)
+
